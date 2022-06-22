96555899a37409d18948b821c30d75cc5e9aeaa3
Labeeb Adegoke <Lharbeeb@gmail.com>
2022-06-22 20:25:07 +0100

    project commit

---
+++ life.js
@@ -0,0 +1,10 @@
+var name = "Labeeb Adegoke";
+var height = 175;
+var country = "Nigeria";
+console.log(name);
+console.log(height);
+console.log(country);
+
+alert(name);
+alert(height);
+alert(country);
